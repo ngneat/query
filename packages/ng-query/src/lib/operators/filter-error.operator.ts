4f9fdd9e6474fcf6c7685a518f9eaa52517dcb5d
rskendzic <radovan.skendzic@symphony.is>
2022-10-09 14:40:31 +0200

    add type guard infering data is null in filterError

--- packages/ng-query/src/lib/operators/filter-error.operator.ts
+++ packages/ng-query/src/lib/operators/filter-error.operator.ts
@@ -2,9 +2,14 @@ import { filter, Observable } from 'rxjs';
 import { NgQueryObserverResult } from '../types';
 
 export function filterError() {
-  return function <T>(
-    source: Observable<NgQueryObserverResult<T>>
-  ): Observable<NgQueryObserverResult<T>> {
-    return source.pipe(filter((result) => result.status === 'error'));
+  return function(
+    source: Observable<NgQueryObserverResult<null>>
+  ): Observable<NgQueryObserverResult<null>> {
+    return source.pipe(
+      filter(
+        (result): result is NgQueryObserverResult<null> =>
+          result.status === 'error' && result.data === null
+      )
+    );
   };
 }
