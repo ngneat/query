7fedf6a082751394495f72ad64514da6ff9d59ac
Duc Nguyen <nhducit@gmail.com>
2022-10-15 00:16:57 +0700

    Move the devtool code to a separated repo

---
+++ packages/ng-query-devtools/README.md
@@ -0,0 +1,11 @@
+# ng-query-devtools
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build ng-query-devtools` to build the library.
+
+## Running unit ng-query-devtoolss
+
+Run `nx ng-query-devtools ng-query-devtools` to execute the unit ng-query-devtoolss via [Jest](https://jestjs.io).
---
+++ packages/ng-query-devtools/jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'ng-query-devtools',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/ng-query-devtools',
+};
