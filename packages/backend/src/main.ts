ceb777d1018cc185be318e7002e27359694399e1
Trung Vo <trungk18@gmail.com>
2022-10-16 22:19:43 +0800

    feat: add deploy gh-pages action

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -31,6 +31,6 @@ app.post('/todos', (req, res) => {
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Listening at http://localhost:${port}`);
 });
 server.on('error', console.error);
