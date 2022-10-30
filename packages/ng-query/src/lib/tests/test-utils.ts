d13410c7f8dcb7a3007d06d092379de0d0f94083
Zargham Khan <zarghamkhan980@gmail.com>
2022-10-30 19:36:19 +0100

    feat(tests): add test for persisted query

--- packages/ng-query/src/lib/tests/test-utils.ts
+++ packages/ng-query/src/lib/tests/test-utils.ts
@@ -1,8 +1,14 @@
-import { Observable, of, throwError } from 'rxjs';
+import { QueryKey } from '@tanstack/query-core';
+import { Observable, of, throwError, delay } from 'rxjs';
+import { queryOptions } from '../persisted-query';
 import { ObservableQueryFn } from '../types';
 
 export function simpleFetcher(): Observable<string> {
   return of('data');
+}
+
+export function delayFetcher(time: number): ObservableQueryFn<string> {
+  return () => of('data').pipe(delay(time));
 }
 
 export function fetcher<T>(data: T): ObservableQueryFn<T> {
@@ -19,13 +25,15 @@ export function flushPromises(timeout = 0): Promise<unknown> {
   });
 }
 
+export type Page = {
+  data: { name: string; id: number }[];
+  nextId: number | null;
+  previousId: number | null;
+};
+
 export function infiniteFetcher(cursor: number, pageSize: number = 5) {
   return () => {
-    return new Observable<{
-      data: { name: string; id: number }[];
-      nextId: number | null;
-      previousId: number | null;
-    }>((observer) => {
+    return new Observable<Page>((observer) => {
       const data = Array(pageSize)
         .fill(0)
         .map((_, i) => {
@@ -51,3 +59,10 @@ export function successMutator<T>(param: T): Observable<T> {
 export function errorMutator<T>(_: T): Observable<Error> {
   return throwError(() => new Error('some error'));
 }
+
+export const persistedFetcher = (queryKey: QueryKey) => {
+  return queryOptions<Page>({
+    queryKey,
+    queryFn: infiniteFetcher(queryKey[1] as number),
+  });
+};
