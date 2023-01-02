916a17f9bf6ada8d7d89e2b589b4228308472d17
NetanelBasal <netanel7799@gmail.com>
2023-01-02 14:29:41 +0200

    fix(query): 🐞 remove logs

--- packages/ng-query/src/lib/base-query.ts
+++ packages/ng-query/src/lib/base-query.ts
@@ -75,7 +75,6 @@ export function baseQuery<
 
         (mergedOptions as any)['queryFn']?.[SUBSCRIPTION]?.unsubscribe();
         queryObserverDispose();
-        console.log((mergedOptions as any)['queryFn']?.[SUBSCRIPTION]);
       };
     }).pipe(
       shareReplay({
