26ec83d6c200164df9428c5dc216d60887b3d968
Giordano Paruccini <donbabbeo@outlook.com>
2024-01-21 12:52:16 +0100

    feat(query): implement functional queryClientOptions factory

--- query/src/lib/query-client-options.ts
+++ query/src/lib/query-client-options.ts
@@ -1,4 +1,8 @@
-import { InjectionToken, makeEnvironmentProviders } from '@angular/core';
+import {
+  EnvironmentProviders,
+  InjectionToken,
+  makeEnvironmentProviders,
+} from '@angular/core';
 import { QueryClientConfig } from '@tanstack/query-core';
 
 export const QUERY_CLIENT_OPTIONS = new InjectionToken<QueryClientConfig>(
@@ -11,11 +15,26 @@ export const QUERY_CLIENT_OPTIONS = new InjectionToken<QueryClientConfig>(
   },
 );
 
-export function provideQueryClientOptions(options: QueryClientConfig) {
+export type QueryClientConfigFn = () => QueryClientConfig;
+
+export function provideQueryClientOptions(
+  options: QueryClientConfig,
+): EnvironmentProviders;
+export function provideQueryClientOptions(
+  options: QueryClientConfigFn,
+): EnvironmentProviders;
+export function provideQueryClientOptions(
+  options: QueryClientConfig | QueryClientConfigFn,
+) {
   return makeEnvironmentProviders([
-    {
-      provide: QUERY_CLIENT_OPTIONS,
-      useValue: options,
-    },
+    typeof options === 'function'
+      ? {
+          provide: QUERY_CLIENT_OPTIONS,
+          useFactory: options,
+        }
+      : {
+          provide: QUERY_CLIENT_OPTIONS,
+          useValue: options,
+        },
   ]);
 }
